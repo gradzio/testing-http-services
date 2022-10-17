bfd96def4962c2142f9b865bdc1a5c42a15d439d
gradzio <gregradzio@gmail.com>
2022-10-17 09:20:49 +0200

    updated client

--- src/app/examples/advanced/clients/api.client.ts
+++ src/app/examples/advanced/clients/api.client.ts
@@ -1,7 +1,62 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import {
+  HttpClient,
+  HttpContext,
+  HttpHandler,
+  HttpHeaders,
+  HttpParams,
+  HttpRequest,
+} from '@angular/common/http';
+import { Inject, Injectable, InjectionToken } from '@angular/core';
+import { environment } from '../../../../environments/environment';
+
+export const API_BASE_URL = new InjectionToken<string>('API_BASE_URL');
+
+export const provideBaseUrl = {
+  provide: API_BASE_URL,
+  useValue: environment.apiUrl
+}
 
 @Injectable()
 export class ApiClient extends HttpClient {
+  constructor(
+    @Inject(API_BASE_URL) private readonly _baseUrl: string,
+    private readonly _handler: HttpHandler
+  ) {
+    super(_handler);
+  }
 
+  override request(
+    first: string | HttpRequest<any>,
+    url?: string,
+    options: {
+      body?: any;
+      headers?: HttpHeaders | { [header: string]: string | string[] };
+      context?: HttpContext;
+      observe?: 'body' | 'events' | 'response';
+      params?:
+        | HttpParams
+        | {
+        [param: string]:
+          | string
+          | number
+          | boolean
+          | ReadonlyArray<string | number | boolean>;
+      };
+      reportProgress?: boolean;
+      responseType?: 'arraybuffer' | 'blob' | 'json' | 'text';
+      withCredentials?: boolean;
+    } = {}
+  ): Observable<any> {
+    if (first instanceof HttpRequest) {
+      const clone = first.clone({
+        url: `${this._baseUrl}${first.url}`,
+      });
+      return super.request(clone);
+    }
+
+    // first is an instance of string (an HTTP method), so we can be sure that url != null because it would not match any signature
+    return super.request(first, `${this._baseUrl}${url}`, options);
+  }
 }
+
--- src/app/examples/advanced/services/http-advanced-products.service-module.ts
+++ src/app/examples/advanced/services/http-advanced-products.service-module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { HttpAdvancedProductsService } from './http-advanced-products.service';
 import { AdvancedProductFactory } from '../factories/advanced-product.factory';
 import { defaultConfigProvider } from '../configs/default.config';
-import { ApiClient } from '../clients/api.client';
+import { ApiClient, provideBaseUrl } from '../clients/api.client';
 
 @NgModule({
   imports: [],
   declarations: [],
-  providers: [HttpAdvancedProductsService, ApiClient, AdvancedProductFactory, defaultConfigProvider],
+  providers: [HttpAdvancedProductsService, ApiClient, provideBaseUrl, AdvancedProductFactory, defaultConfigProvider],
   exports: []
 })
 export class HttpAdvancedProductsServiceModule {
